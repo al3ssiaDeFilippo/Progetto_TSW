10c1ff859263cb4be75d33e71b057a888d28dbe3
Biagio Gallo <biagiogallo03@gmail.com>
2024-05-08 17:46:24 +0200

    Abbiamo aggiunto il carrello, il tasto per rimuovere un elemento ma non funge.

--- db/storage.sql
+++ db/storage.sql
@@ -2,18 +2,26 @@ DROP DATABASE IF EXISTS storage;
 CREATE DATABASE storage;
 USE storage;
 
-DROP TABLE IF EXISTS product;
 
-CREATE TABLE product (
-                         code int primary key auto_increment default 0,
-                         productName varchar(20) not null,
-                         details varchar(200) not null,
-                         quantity int default 0 not null,
-                         category varchar(8) check(category in('Film', 'Anime', 'Fumetti', 'Giochi', 'Serie TV')) not null,
-                         price float default 0 not null,
-                         iva int default 0 not null,
-                         discount int default 0
+CREATE TABLE product (	
+  code int primary key AUTO_INCREMENT,
+  productName char(20) not null,
+  details char(200),
+  quantity int default 0,
+  category varchar(8) check(category in('Film', 'Anime', 'Fumetti', 'Giochi', 'Serie TV')) not null,
+  price float default 0 not null,
+  iva int default 0 not null,
+  discount int default 0,
+  frame varchar(8) check(frame in('no frame', 'wood', 'PVC')) not null,
+  frameColor varchar(5) check(frameColor in ('black', 'brown', 'white')) not null,
+  size varchar(5) check(size in('21x30', '85x60', '91x61')) not null
 );
 
-INSERT INTO product (productName, details, quantity, category, price, iva) VALUES ('Crash Bandicoot', 'Crash Bandicoot', 3, 'Giochi', 15.8, 22);
-INSERT INTO product (productName, details, quantity, category, price, iva) VALUES ('Spiderman VS Goblin', 'Spiderman VS Goblin', 6, 'Fumetti', 47, 22);
+
+INSERT INTO productDetails (productCode, frame, frameColor, size)
+VALUES (1, 'wood', 'brown', '85x60');
+INSERT INTO productDetails (productCode, frame, frameColor, size)
+VALUES (2, 'PVC', 'white', '91x61');
+
+INSERT INTO product (productName, details, quantity, category, price, iva, frame, frameColor, size) VALUES ('Crash Bandicoot', 'Crash Bandicoot', 3, 'Giochi', 15.8, 22, 'wood', 'brown', '85x60');
+INSERT INTO product (productName, details, quantity, category, price, iva, frame, frameColor, size) VALUES ('Spiderman VS Goblin', 'Spiderman VS Goblin', 6, 'Fumetti', 47, 22, 'PVC', 'white', '91x61');
