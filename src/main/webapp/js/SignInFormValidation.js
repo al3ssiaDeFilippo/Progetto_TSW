15f97365b4e8db1129c2c1e93765da5ab7698915
Alessia De Filippo <alessiadefilippo93@gmail.com>
2024-07-11 08:28:16 +0200

    modifica del file SignInFormValidation.js

--- src/main/webapp/js/SignInFormValidation.js
+++ src/main/webapp/js/SignInFormValidation.js
@@ -102,7 +102,7 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 
     //cognome: almeno un carattere, massimo 20 caratteri
-   function validateSurname(surname) {
+    function validateSurname(surname) {
         if(surname.length <= 0) {
             return "Il cognome deve essere di almeno un carattere"
         }
@@ -110,93 +110,93 @@ document.addEventListener("DOMContentLoaded", function() {
         if(surname.length > 20) {
             return "Il cognome deve essere di masssimo 20 caratteri"
         }
-   }
+    }
 
     //nome: almeno un carattere, massimo 20 caratteri
-   function validateName(name) {
-         if(name.length <= 0) {
-              return "Il nome deve essere di almeno un carattere"
-         }
+    function validateName(name) {
+        if(name.length <= 0) {
+            return "Il nome deve essere di almeno un carattere"
+        }
 
-         if(name.length > 20) {
-              return "Il nome deve essere di masssimo 20 caratteri"
-         }
-   }
+        if(name.length > 20) {
+            return "Il nome deve essere di masssimo 20 caratteri"
+        }
+    }
 
-   //data di nascita: deve essere una data valida e l'utente deve avere almeno 18 anni
-   function validateBirthDate(BirthDate) {
-       const today = new Date();
-       const birthDate = new Date(BirthDate);
+    //data di nascita: deve essere una data valida e l'utente deve avere almeno 18 anni
+    function validateBirthDate(BirthDate) {
+        const today = new Date();
+        const birthDate = new Date(BirthDate);
 
-       const birthYear = birthDate.getFullYear();
-       if(birthYear < 1900 || birthYear >= today.getFullYear()) {
-              return "La data di nascita non è valida";
-       }
+        const birthYear = birthDate.getFullYear();
+        if(birthYear < 1900 || birthYear >= today.getFullYear()) {
+            return "La data di nascita non è valida";
+        }
 
-       let age = today.getFullYear() - birthDate.getFullYear();
-       const monthDifference = today.getMonth() - birthDate.getMonth();
-       const dayDifference = today.getDate() - birthDate.getDate();
+        let age = today.getFullYear() - birthDate.getFullYear();
+        const monthDifference = today.getMonth() - birthDate.getMonth();
+        const dayDifference = today.getDate() - birthDate.getDate();
 
-       if(monthDifference < 0 || (monthDifference === 0 && dayDifference < 0)) {
-           age--;
-       }
+        if(monthDifference < 0 || (monthDifference === 0 && dayDifference < 0)) {
+            age--;
+        }
 
-       if(age <= 18) {
+        if(age <= 18) {
             return "Devi avere almeno 18 anni per registrarti";
-       }
-   }
+        }
+    }
 
-   //email: deve essere una email valida e non deve essere già in uso
-   function validateEmail(email) {
-       const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-       const isNotUsed = email !== document.getElementById("email").value;
-       if(isNotUsed) {
-              return "L'email è già in uso";
-       }
-       if(emailPattern.test(email) <= 50) {
-                return "L'email non è valida";
-       }
+    //email: deve essere una email valida e non deve essere già in uso
+    function validateEmail(email) {
+        const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+        const isNotUsed = email !== document.getElementById("email").value;
+        if(isNotUsed) {
+            return "L'email è già in uso";
+        }
+        if(emailPattern.test(email) <= 50) {
+            return "L'email non è valida";
+        }
         return "";
-   }
+    }
 
     //controlli sulla password
-   function validatePassword(password) {
-       const hasLowerCase = /[a-z]/.test(password);
-       if(hasLowerCase) {
-           return "La password deve contenere almeno una lettera minuscola"
-       }
-       const hasUpperCase = /[A-Z]/.test(password)
-       if(hasUpperCase) {
-           return "La password deve contenere almeno una lettera maiuscola"
-       }
-       const hasNumber = /[0-9]/.test(password);
-       if(hasNumber) {
-           return "La password deve contenere almeno un numero"
-       }
-       const hasSpecialCharacter = /[!@#$%^&*]/.test(password);
-       if(hasSpecialCharacter) {
-           return "La password deve contenere almeno un carattere speciale"
-       }
-       const isNotUsername = password !== document.getElementById("username").value;
-       if(isNotUsername) {
-           return "La password non può essere uguale all'username"
-       }
-       const isTooShort = password.length >= 8;
-       if(isTooShort) {
-           return "La password deve essere di almeno 8 caratteri"
-       }
-       const isLongEnough = password.length <= 20;
-       if(isLongEnough) {
-           return "La password deve essere di massimo 20 caratteri"
-       }
+    function validatePassword(password) {
+        const hasLowerCase = /[a-z]/.test(password);
+        if(hasLowerCase) {
+            return "La password deve contenere almeno una lettera minuscola"
+        }
+        const hasUpperCase = /[A-Z]/.test(password)
+        if(hasUpperCase) {
+            return "La password deve contenere almeno una lettera maiuscola"
+        }
+        const hasNumber = /[0-9]/.test(password);
+        if(hasNumber) {
+            return "La password deve contenere almeno un numero"
+        }
+        const hasSpecialCharacter = /[!@#$%^&*]/.test(password);
+        if(hasSpecialCharacter) {
+            return "La password deve contenere almeno un carattere speciale"
+        }
+        const isNotUsername = password !== document.getElementById("username").value;
+        if(isNotUsername) {
+            return "La password non può essere uguale all'username"
+        }
+        const isTooShort = password.length >= 8;
+        if(isTooShort) {
+            return "La password deve essere di almeno 8 caratteri"
+        }
+        const isLongEnough = password.length <= 20;
+        if(isLongEnough) {
+            return "La password deve essere di massimo 20 caratteri"
+        }
 
-       return "";
-   }
+        return "";
+    }
 
-   function validateTelNumber(telNumber) {
-       const telNumberPattern = /^[0-9]{10}$/;
-       if(telNumberPattern || telNumber.length !== 10) {
-           return "Numero di telefono non valido"
-       }
-   }
+    function validateTelNumber(telNumber) {
+        const telNumberPattern = /^[0-9]{10}$/;
+        if(telNumberPattern || telNumber.length !== 10) {
+            return "Numero di telefono non valido"
+        }
+    }
 });
